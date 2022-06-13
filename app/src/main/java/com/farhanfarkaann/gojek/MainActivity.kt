d80439ea8cb7632858ae3c6a38e18501a26b9ccf
Farhan160200 <fadhlurrahmanfarhan16@gmail.com>
2022-06-13 21:51:05 +0700

    testing bentrok

--- app/src/main/java/com/farhanfarkaann/gojek/MainActivity.kt
+++ app/src/main/java/com/farhanfarkaann/gojek/MainActivity.kt
@@ -7,8 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         ///testing branch 1
-    }
 
+
+
+
+
+        // tesingg bentrok
+    }
 }
